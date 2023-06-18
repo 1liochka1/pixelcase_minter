54a92a4f09ae4df48126a78b071e36437fb02ef8
1liochka1 <ila00001120@gmail.com>
2023-06-18 22:57:57 +0400

    fix

--- main.py
+++ main.py
@@ -7,18 +7,22 @@ import pandas as pd
 from config import *
 
 
-def check_status_tx(tx_hash, w3):
-    logger.info(f'waiting for confirmation transaction https://etherscan.io/tx/{w3.to_hex(tx_hash)}...')
+def check_status_tx(w3, tx_hash):
+    logger.info(f'жду подтверждения транзакции - https://explorer.zksync.io/tx/{w3.to_hex(tx_hash)}...')
+
+    start_time = int(time.time())
     while True:
+        current_time = int(time.time())
+        if current_time >= start_time + 100:
+            logger.info('транзакция не подтвердилась за 100 cекунд, начинаю повторную отправку...')
+            return 0
         try:
-            status = w3.eth.get_transaction_receipt(tx_hash)
-            status = status['status']
-            if status in [0, 1]:
+            status = w3.eth.get_transaction_receipt(tx_hash)['status']
+            if status == 1:
                 return status
             time.sleep(1)
         except Exception as error:
             time.sleep(1)
-
 
 def sleep_indicator(secs):
     for i in tqdm(range(secs), desc='wait', bar_format="{desc}: {n_fmt}s / {total_fmt}s {bar}", colour='green'):
@@ -45,11 +49,14 @@ def mint(privatekey):
         tx['gas'] = w3.eth.estimate_gas(tx)
         sign = account.sign_transaction(tx)
         hash_ = w3.eth.send_raw_transaction(sign.rawTransaction)
-        status = check_status_tx(hash_, w3)
+        status = check_status_tx(w3, hash_)
         if status == 1:
             logger.success(f'{address} - успешно заминтил: https://explorer.zksync.io/tx/{w3.to_hex(hash_)}...')
             sleep_indicator(random.randint(delay[0], delay[1]))
             return address, 'success'
+        else:
+            logger.info(f'{address} - пробую еще раз...')
+            mint(privatekey)
 
     except Exception as e:
         error = str(e)
